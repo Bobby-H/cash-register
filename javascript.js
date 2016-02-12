4d42d249854efb60d7110655636a336e756d3724
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-02-12 04:33:30 -0500

    added scanner and item case statement

--- javascript.js
+++ javascript.js
@@ -2,10 +2,20 @@ var cashRegister = {
   total: 2.99,
   add: function(itemCost){
     this.total += itemCost;
+  },
+  scan: function(item){
+    switch (item) {
+      case "eggs": this.add(0.98); break
+      case "milk": this.add(1.23); break
+      case "magazine": this.add(4.99); break
+      case "chocolate": this.add(0.45); break
+    }
+    return true;
   }
 }
-cashRegister.add(1.11);
-cashRegister.add(2.45);
-cashRegister.add(3.93);
-cashRegister.add(4.76);
+cashRegister.scan("eggs");
+cashRegister.scan("milk");
+cashRegister.scan("magazine");
+cashRegister.scan("chocolate");
+cashRegister.scan("chocolate");
 console.log('your total is ' + cashRegister.total);
