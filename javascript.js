4a4196601492f6a11b2978e27f2c43f93336cb96
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-02-12 03:02:30 -0500

    added add function to cashregister object

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
 var cashRegister = {
-  total: 2.99
+  total: 2.99,
+  add: function(itemCost){
+    this.total += itemCost;
+  }
 }
+cashRegister.add(1.11);
+cashRegister.add(2.45);
+cashRegister.add(3.93);
+cashRegister.add(4.76);
+console.log('your total is ' + cashRegister.total);
